182653a1c6b8aca04e614d277333f1cc7c18238e
Bnyro <bnyro@tutanota.com>
2023-01-19 19:55:34 +0100

    Add default values and base path option

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use actix_web::{App, HttpServer};
 
 #[derive(Parser)]
 struct Cli {
+    #[arg(short, long, default_value = ".")]
     path: String,
+    #[arg(short, long, default_value = "/")]
+    base: String,
 }
 
 #[actix_web::main]
@@ -16,7 +19,7 @@ async fn main() -> std::io::Result<()> {
         std::process::exit(exitcode::OK);
     }).expect("Error setting Crtl-C handler");
 
-    HttpServer::new(move || App::new().service(fs::Files::new("/", args.path.as_str()).show_files_listing()))
+    HttpServer::new(move || App::new().service(fs::Files::new(args.base.as_str(), args.path.as_str()).show_files_listing()))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
