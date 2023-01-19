62050cd3147803192be6b52ec3cbeb821d38e169
Bnyro <bnyro@tutanota.com>
2023-01-19 19:48:05 +0100

    Stop process on crtc-c

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ struct Cli {
 async fn main() -> std::io::Result<()> {
     let args = Cli::parse();
 
+    ctrlc::set_handler(|| {
+        println!("\nShutting down live server. See you later!");
+        std::process::exit(0x0100);
+    }).expect("Error setting Crtl-C handler");
+
     HttpServer::new(move || App::new().service(fs::Files::new("/", args.path.as_str()).show_files_listing()))
         .bind(("127.0.0.1", 8080))?
         .run()
