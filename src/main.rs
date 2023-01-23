6f12d0e89abf3f641dc4262710ce0577c25f795b
Bnyro <bnyro@tutanota.com>
2023-01-23 19:46:58 +0100

    Improve the syntax

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{path::{Path, PathBuf}, fs};
 
 use clap::{Parser, ArgAction, ValueHint};
-use actix_web::{App, HttpServer, web, HttpRequest, HttpResponse};
+use actix_web::{App, HttpServer, web, HttpResponse, get, http::header::ContentType};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,18 +18,21 @@ struct Cli {
     directory: Option<String>,
 }
 
-async fn index(req: HttpRequest) -> HttpResponse {
-    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+#[get("/{filename:.*}")]
+async fn index(filename: web::Path<String>) -> HttpResponse {
+    let path: PathBuf = filename.parse().unwrap();
     let base = PathBuf::from("/home/bnyro/Projects/bnyro.github.io/");
     let target = base.join(path);
+
+    let mut content_type = ContentType::plaintext().to_string();
+    let mut content = String::from("Not Found");
     if target.is_file() {
-        let content_type = mime_guess::from_path(target.clone()).first().expect("No Mime Type found");
-        let content = fs::read_to_string(target).expect("Can't read file!");
-        return HttpResponse::Ok().content_type(content_type).body(content);
+        content_type = mime_guess::from_path(target.clone()).first().expect("No Mime Type found").to_string();
+        content = fs::read_to_string(target).expect("Can't read file!");
     } else if target.is_dir() {
-        return HttpResponse::Ok().body(String::from("Directory, todo"));
+        content = String::from("Directory: // TODO");
     }
-    HttpResponse::NotFound().body("Not Found")
+    return HttpResponse::Ok().content_type(content_type).body(content);
 }
 
 #[actix_web::main]
@@ -49,7 +52,7 @@ async fn main() -> std::io::Result<()> {
 
     println!("\nStarted live server at {}:{}", "http://".to_string() + args.address.replace("http://", "").as_str(), args.port);
     HttpServer::new(|| {
-        App::new().route("/{filename:.*}", web::get().to(index))
+        App::new().service(index)
     })
         .bind((args.address, args.port))?
         .run()
