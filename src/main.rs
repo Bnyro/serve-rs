9da3914e930edbfde9c811baee97abb7ceb0f0f1
Bnyro <bnyro@tutanota.com>
2023-01-23 19:27:12 +0100

    Get rid of actix files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::path::Path;
+use std::{path::{Path, PathBuf}, fs};
 
 use clap::{Parser, ArgAction, ValueHint};
-use actix_files as fs;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web, HttpRequest, HttpResponse};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -17,6 +16,20 @@ struct Cli {
     list: bool,
     #[arg(value_hint = ValueHint::DirPath)]
     directory: Option<String>,
+}
+
+async fn index(req: HttpRequest) -> HttpResponse {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    let base = PathBuf::from("/home/bnyro/Projects/bnyro.github.io/");
+    let target = base.join(path);
+    if target.is_file() {
+        let content_type = mime_guess::from_path(target.clone()).first().expect("No Mime Type found");
+        let content = fs::read_to_string(target).expect("Can't read file!");
+        return HttpResponse::Ok().content_type(content_type).body(content);
+    } else if target.is_dir() {
+        return HttpResponse::Ok().body(String::from("Directory, todo"));
+    }
+    HttpResponse::NotFound().body("Not Found")
 }
 
 #[actix_web::main]
@@ -35,13 +48,8 @@ async fn main() -> std::io::Result<()> {
     }
 
     println!("\nStarted live server at {}:{}", "http://".to_string() + args.address.replace("http://", "").as_str(), args.port);
-    HttpServer::new(move || {
-        let files = fs::Files::new(args.base.as_str(), path.as_str());
-        let service = match args.list {
-            true => files.show_files_listing(),
-            false => files,
-        };
-        App::new().service(service)
+    HttpServer::new(|| {
+        App::new().route("/{filename:.*}", web::get().to(index))
     })
         .bind((args.address, args.port))?
         .run()
