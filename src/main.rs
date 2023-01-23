8272088a15b6d406261fe2e2c1982e2d4e0db203
Bnyro <bnyro@tutanota.com>
2023-01-23 20:25:54 +0100

    List directories

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{path::{Path, PathBuf}, fs};
+use std::{path::{Path, PathBuf}, fs::{self}};
 
 use clap::{Parser, ArgAction, ValueHint};
 use actix_web::{App, HttpServer, web, HttpResponse, get, http::header::ContentType};
@@ -22,7 +22,7 @@ struct Cli {
 async fn index(filename: web::Path<String>) -> HttpResponse {
     let path: PathBuf = filename.parse().unwrap();
     let base = PathBuf::from("/home/bnyro/Projects/bnyro.github.io/");
-    let target = base.join(path);
+    let target = base.join(path.clone());
 
     let mut content_type = ContentType::plaintext().to_string();
     let mut content = String::from("Not Found");
@@ -30,9 +30,50 @@ async fn index(filename: web::Path<String>) -> HttpResponse {
         content_type = mime_guess::from_path(target.clone()).first().expect("No Mime Type found").to_string();
         content = fs::read_to_string(target).expect("Can't read file!");
     } else if target.is_dir() {
-        content = String::from("Directory: // TODO");
+        content_type = ContentType::html().to_string();
+        content = String::from(directory_listing(target));
     }
     return HttpResponse::Ok().content_type(content_type).body(content);
+}
+
+fn directory_listing(base: PathBuf) -> String {
+    let children = base.read_dir().unwrap();
+
+    let mut body = String::new();
+
+    for entry in children {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            let p = match path.strip_prefix(&base) {
+                Ok(p) if cfg!(windows) => base.join(p).to_string_lossy().replace('\\', "/"),
+                Ok(p) => base.join(p).to_string_lossy().into_owned(),
+                Err(_) => continue,
+            };
+
+            if path.is_dir() {
+                body += &format!(
+                    "<li><a href=\"{}\">{}/</a></li>",
+                    p,
+                    entry.file_name().to_string_lossy(),
+                ).to_string();
+            } else {
+                body += &format!(
+                    "<li><a href=\"{}\">{}</a></li>",
+                    p,
+                    entry.file_name().to_string_lossy(),
+                ).to_string();
+            }
+        }
+
+    format!(
+        "<html>\
+         <head><title>{}</title></head>\
+         <body><h1>{}</h1>\
+         <ul>\
+         {}\
+         </ul></body>\n</html>",
+        "", "", body
+    )
 }
 
 #[actix_web::main]
