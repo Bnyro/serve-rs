6111243060743bb93c862f3df51b0185b41f6618
Bnyro <bnyro@tutanota.com>
2024-03-03 17:34:07 +0100

    chore: simplify and refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 pub mod directory;
 
-use std::{path::{Path, PathBuf}, fs::File, io::Read};
+use std::{
+    fs::File,
+    io::Read,
+    path::{Path, PathBuf},
+};
 
-use clap::{Parser, ArgAction, ValueHint};
-use actix_web::{App, HttpServer, web, HttpResponse, get, http::header::ContentType};
+use actix_web::{get, http::header::ContentType, web, App, HttpResponse, HttpServer};
+use clap::{ArgAction, Parser, ValueHint};
 
 use crate::directory::directory_listing;
 use lazy_static::lazy_static;
@@ -31,39 +35,38 @@ lazy_static! {
 
 #[get("/{filename:.*}")]
 async fn index(filename: web::Path<String>) -> HttpResponse {
-    
     let base = PathBuf::from(ARGS.directory.clone().unwrap_or(String::from(".")));
-    let target = if filename.to_string().is_empty() && ARGS.index_path.is_some() {
-        base.join(ARGS.index_path.clone().unwrap())
+    let path = if filename.to_string().is_empty() && ARGS.index_path.is_some() {
+        ARGS.index_path.clone().unwrap()
     } else {
-        let path: PathBuf = filename.parse().unwrap();
-        base.join(path.clone())
+        filename.parse().unwrap()
     };
+    let target = base.join(path);
 
     let mut content_type = ContentType::plaintext().to_string();
     let mut response_bytes: Vec<u8> = String::from("Not Found").into_bytes();
     if target.is_file() {
-        match mime_guess::from_path(target.clone()).first() {
-            Some(ct) => {
-                content_type = ct.to_string();
-            },
-            None => {}
+        if let Some(ct) = mime_guess::from_path(target.clone()).first() {
+            content_type = ct.to_string();
         }
         let mut file_content = Vec::new();
-        File::open(target).expect("Unable to open file").read_to_end(& mut file_content).expect("Can't read file!");
+        File::open(target)
+            .expect("Unable to open file")
+            .read_to_end(&mut file_content)
+            .expect("Can't read file!");
         response_bytes = file_content;
     } else if target.is_dir() {
         match ARGS.list {
             true => {
                 content_type = ContentType::html().to_string();
                 response_bytes = directory_listing(target, base).into_bytes();
-            },
-            false => {
-                return HttpResponse::NotFound().body("Page Not Found")
             }
+            false => return HttpResponse::NotFound().body("Page Not Found"),
         }
     }
-    return HttpResponse::Ok().content_type(content_type).body(response_bytes);
+    return HttpResponse::Ok()
+        .content_type(content_type)
+        .body(response_bytes);
 }
 
 #[actix_web::main]
@@ -71,7 +74,8 @@ async fn main() -> std::io::Result<()> {
     ctrlc::set_handler(|| {
         println!("\nShutting down live server. See you later!");
         std::process::exit(exitcode::OK);
-    }).expect("Error setting Crtl-C handler");
+    })
+    .expect("Error setting Crtl-C handler");
 
     let path = ARGS.directory.clone().unwrap_or(String::from("."));
     if !Path::new(path.clone().as_str()).exists() {
@@ -79,7 +83,11 @@ async fn main() -> std::io::Result<()> {
         std::process::exit(exitcode::OSFILE);
     }
 
-    println!("Started live server at {}:{}", "http://".to_string() + ARGS.address.replace("http://", "").as_str(), ARGS.port.clone());
+    println!(
+        "Started live server at {}:{}",
+        "http://".to_string() + ARGS.address.replace("http://", "").as_str(),
+        ARGS.port.clone()
+    );
     HttpServer::new(move || App::new().service(index))
         .bind((ARGS.address.clone(), ARGS.port))?
         .run()
